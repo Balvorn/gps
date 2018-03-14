7a466008116a74fccdf162a9c8ef2c521f4c40cf
kurosaki-88 <jack@djekboubdesign.com>
2018-03-14 13:43:09 +0100

    v1.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,28 @@ import * as EXIF from 'exif-js';
 })
 export class AppComponent {
     title = 'My first AGM project';
-
+    lat: number ;
+    lng: number ;
+    show: boolean = false;
 
     getExif() {
         let img = document.getElementById('img');
         let result = document.getElementById('result');
-        lat = EXIF.getData(img, function () {
+        this.show = true;
+        EXIF.getData(img, function () {
 
             this.DMSLat = EXIF.getTag(img, 'GPSLatitude');
             this.lat = this.DMSLat[0] + (((this.DMSLat[1] * 60) + (this.DMSLat[2])) / 3600);
+
+            console.log(this.lat);
         });
-        lon = EXIF.getData(img, function () {
+
+        EXIF.getData(img, function () {
 
             this.DSMLon = EXIF.getTag(img, 'GPSLongitude');
             this.lng = this.DSMLon[0] + (((this.DSMLon[1] * 60) + (this.DSMLon[2])) / 3600);
-
+            this.show = true;
+            console.log(this.lng);
         });
     }
 
