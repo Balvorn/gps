5fedcf29b0ef7d6e1a3ddcdb5c38e50740e48e46
Balvorn <leo-marcou@hotmail.fr>
2018-03-27 16:02:42 +0200

    multi-markers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ApplicationRef } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import * as exif from 'exif-js'
-import { AgmCoreModule } from '@agm/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {AppComponent} from './app.component';
+// import * as EXIF from 'exif-js';
+import {AgmCoreModule} from '@agm/core';
+import { AgmJsMarkerClustererModule } from '@agm/js-marker-clusterer';
 
 @NgModule({
     imports: [
@@ -13,10 +14,14 @@ import { AgmCoreModule } from '@agm/core';
         FormsModule,
         AgmCoreModule.forRoot({
             apiKey: 'AIzaSyBcHhfeJ0iNJAyfV8-m6eknRx_RcG6EpMM'
-        })
+        }),
+        AgmJsMarkerClustererModule
     ],
     providers: [],
-    declarations: [ AppComponent ],
-    bootstrap: [ AppComponent ]
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+
+export class AppModule {
+
+}
